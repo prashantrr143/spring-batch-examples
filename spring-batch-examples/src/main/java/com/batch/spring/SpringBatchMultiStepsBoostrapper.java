eea6022640820d3c0273e8fba82ea5f111b4c418
prasingh26 <prasingh26@WKWIN1194166.global.publicisgroupe.net>
2018-07-31 15:13:17 +0530

    refactoring package naming and structure

---
+++ spring-batch-examples/src/main/java/com/batch/spring/SpringBatchMultiStepsBoostrapper.java
@@ -0,0 +1,20 @@
+package com.batch.spring;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Profile;
+
+
+
+@SpringBootApplication
+@EnableBatchProcessing
+
+@Profile({"steps","csv-xml"})
+public class SpringBatchMultiStepsBoostrapper {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchMultiStepsBoostrapper.class, args);
+	}
+}
